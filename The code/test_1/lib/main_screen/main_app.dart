ab6404d0322267af3fd0390f091b3722829a0676
Abdulaziz Alsheikh <abdulazizalsheikh2030@gmail.com>
2024-05-18 02:36:19 +0300

    Edited main_app to have background music.

--- The code/test_1/lib/main_screen/main_app.dart
+++ The code/test_1/lib/main_screen/main_app.dart
@@ -1,17 +1,41 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:test_1/main_screen/main_menu.dart';
+import 'audio_manager.dart';
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget{
+  @override
+  _MainAppState createState() => _MainAppState();
+
+}
+
+class _MainAppState extends State<MainApp> {
+  final AudioManager _audioManager = AudioManager();
+
+  @override
+  void initState() {
+    super.initState();
+    _audioManager.init().then((_) {
+      _audioManager.play();
+    });
+  }
+
+
+@override
+  void dispose() {
+    _audioManager.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'EdRaak',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       home: MainMenu(),
+      
     );
   }
 }
