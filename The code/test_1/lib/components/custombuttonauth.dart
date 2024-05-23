c3c5072a2c3393b29b54b7e404c6508eda31c2fa
Osamah <osamah.saddeq@gmail.com>
2024-05-23 23:30:24 +0300

    Enhancments on UI/UX in login page and sign in page

--- The code/test_1/lib/components/custombuttonauth.dart
+++ The code/test_1/lib/components/custombuttonauth.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class CustomButtonAuth extends StatelessWidget {
   final void Function()? onPressed;
@@ -7,13 +8,22 @@ class CustomButtonAuth extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialButton(
-      height: 40,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-      color: Colors.blue,
-      textColor: Colors.white,
-      onPressed: onPressed,
-      child: Text(title),
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 50),
+      child: MaterialButton(
+        height: 40,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+        color: Colors.blue,
+        textColor: Colors.white,
+        onPressed: onPressed,
+        child: Text(
+          title,
+          style: GoogleFonts.tajawal(
+            fontSize: 17.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
     );
   }
 }
