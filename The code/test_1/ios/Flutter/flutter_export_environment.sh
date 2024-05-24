b912cbc6ee535d377bf41b58c6c7f176e9065f33
Abdulaziz Alsheikh <abdulazizalsheikh2030@gmail.com>
2024-05-24 21:04:36 +0300

    added XCODE ios runner

--- The code/test_1/ios/Flutter/flutter_export_environment.sh
+++ The code/test_1/ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Dev\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\osama\Documents\My-Github\EdRaak\EdRaak\The code\test_1"
+export "FLUTTER_ROOT=/Users/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/azizalsheikh/Desktop/for_git/EdRaak/The code/test_1"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
