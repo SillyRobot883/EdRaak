df51049864098882479a0ce292e229c70978f241
Osamah <osamah.saddeq@gmail.com>
2024-05-29 23:19:40 +0300

    integrate The Firebase with web

--- The code/test_1/lib/main.dart
+++ The code/test_1/lib/main.dart
@@ -6,10 +6,14 @@ import 'package:test_1/main_screen/main_app.dart';
 import 'main_screen/Settings.dart';
 import 'main_screen/audio_manager.dart';
 
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+      options: FirebaseOptions(
+          apiKey: "AIzaSyBJC8DpfK2H9qziqEt1TVNwtMgeeixcZ0E",
+          appId: "1:738177878309:web:db911d332084535d50f2cc",
+          messagingSenderId: "738177878309",
+          projectId: "edraak1-2fb39"));
 
   runApp(
     MultiProvider(
